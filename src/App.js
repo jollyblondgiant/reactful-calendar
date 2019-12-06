5138666154b6dabc97f7f779c91a426cc0a6805f
jollyblondgiant <andrew.carlil3@gmail.com>
2019-12-06 13:20:27 -0500

    added clickable events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import './App.css';
+import "@fullcalendar/timegrid/main.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  calendarComponentRef = React.createRef();
+  
+  state = {
+    calendarEvents: [
+      //takes a vector of maps
+      //assoc event w/ resource: match event.resourceId to resource.id
+      //assoc event w/ resources: match event.resourceIds to resource.id
+      //event.resourceIds is a vector of strings
+      {title: "Closing", 
+       start: new Date(),
+       // resourceId: "roomA"
+       resourceIds: ["roomA", "roomB"],
+       editable: true
+      }
+    ],
+    resources: [
+      {title: "Room A", id: "roomA"},
+      {title: "Room B", id: "roomB"}
+    ]
+  };
+
+  // handleDateClick = arg => {
+  //   console.log(arg)
+  // };
+
+
+  handleDateClick = arg => {
+    if (window.confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+      this.setState({
+        calendarEvents: this.state.calendarEvents.concat({         
+          title: "New Event",
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      });
+    }
+  };
+
+  render () {
+    return (
+      <div className="App">     
+        <header className="App-header">
+          <div id="st-header">
+            <h1><span style={{color: '#0065b3'}}>Short</span>
+            <span style={{color: '#ff9910'}}>Track</span> Calendar</h1>
+          </div>     
+          <FullCalendar 
+            defaultView = "dayGridMonth"
+            header={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,resourceTimeGridDay"
+            }} 
+            dateClick={this.handleDateClick}
+            events={this.state.calendarEvents}
+            resources={this.state.resources}
+            eventClick={this.handleEventClick}
+            schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+            plugins={[resourceTimeGridPlugin, dayGridPlugin, 
+                      interactionPlugin]}
+          />         
+        </header>
+      </div> 
+    );
+  }
+  
+  handleEventClick = info => {
+   alert(info.event.title);
+    
+    
+  }; 
+
 }
 
-export default App;
+
